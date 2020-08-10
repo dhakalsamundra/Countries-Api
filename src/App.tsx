e1e3ad81addbeb3f87dfab8b7d81b3991d686d62
dhakalsamundra <seapush02@gmail.com>
2020-08-10 18:23:03 +0300

    context api for theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Routes from './Routes'
+import themeContext, { themes } from './themeContext'
 
 export default function App() {
+  const [context, setContext] = useState({
+    theme: themes.primary,
+    switchTheme: (code: string) => {
+      // console.log(code)
+      setContext((current) => ({
+        ...current,
+        theme:
+          code === themes.primary.code
+            ? themes.primary
+            : code === themes.secondary.code
+              ? themes.secondary
+              : themes.third,
+      }))
+    },
+  })
   return (
-    <>
+    <themeContext.Provider value={context}>
       <Routes />
-    </>
+    </themeContext.Provider>
   )
 }
