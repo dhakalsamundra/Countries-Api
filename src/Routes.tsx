1ee48d662f6f98c1d76dad8170714c05a067635c
dhakalsamundra <seapush02@gmail.com>
2020-08-10 18:25:26 +0300

    add routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,24 +1,15 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import Home from './pages/Home'
-import Product from './pages/Product'
+import Home from './pages/home'
+import { CountryDetails } from './pages/CountryDetails'
 
 const Routes = () => (
   <Switch>
-    <Route
-      exact
-      path='/'
-      component={ Home }
-    />
-    <Route
-      exact
-      path='/products/:id'
-      component={ Product }
-    />
+    <Route exact path="/" component={Home} />
+
+    <Route exact path="/countries/:name" component={CountryDetails} />
   </Switch>
 )
 
 export default Routes
-
-
