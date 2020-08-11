f02141c9d7fa4f8aef3f30fe87d290bf3096597e
dhakalsamundra <seapush02@gmail.com>
2020-08-11 10:38:16 +0300

    add navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import NavBar from '../Components/NavBar/navBar'
 import CountryLists from '../Components/Country/country'
 
 export default function Home() {
   return (
     <>
+      <NavBar />
       <CountryLists />
     </>
   )
