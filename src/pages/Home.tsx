2854e3d43d27fce26e24f9f806fe4921153cd3df
dhakalsamundra <seapush02@gmail.com>
2020-08-17 23:38:28 +0300

    refactor error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
 import NavBar from '../Components/NavBar/navBar'
-import CountryLists from '../Components/Country/country'
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <CountryLists />
     </>
   )
 }
